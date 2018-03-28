8185b373d31ac21586e5c301c2e6e0d43ff485ef
Bagus Rinaldhi <bagusrinn@gmail.com>
2018-03-28 14:04:56 +0700

    Initial commit

---
+++ src/Bagusrin/SummarizerBundle/BagusrinSummarizerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bagusrin\SummarizerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BagusrinSummarizerBundle extends Bundle
+{
+}
---
+++ src/Bagusrin/SummarizerBundle/Manager/SummarizerManager.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Bagusrin\SummarizerBundle\Manager;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\ProcessBuilder;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+class SummarizerManager
+{
+
+    private $_summarizer = 'summarizer';
+
+    private $_summarizerUrl = 'summarizer_url';
+
+    private $_basePath;
+
+    public function __construct()
+    {
+        $basePath = realpath(dirname(__FILE__)).'/../../../../';
+        $this->_basePath = $basePath.'bin/summarizer/';
+    }
+
+    public function analyze($sentence)
+    {
+        try {
+            $command = "cd {$this->_basePath} && python {$this->_summarizer} \"{$sentence}\"";
+            $process = new Process($command);
+            $process->run();
+
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+
+            $output = $process->getOutput();
+
+            return json_decode($output, true);
+        } catch(ProcessFailedException $e) {
+            return false;
+        } catch(\Exception $e) {
+            return false;
+        }
+    }
+
+    public function analyzeUrl($url)
+    {
+        try {
+            $command = "cd {$this->_basePath} && python {$this->_summarizerUrl} \"{$url}\"";
+            $process = new Process($command);
+            $process->run();
+
+            if (!$process->isSuccessful()) {
+                throw new ProcessFailedException($process);
+            }
+
+            $output = $process->getOutput();
+
+            return json_decode($output, true);
+        } catch(ProcessFailedException $e) {
+            return false;
+        } catch(\Exception $e) {
+            return false;
+        }
+    }
+}
